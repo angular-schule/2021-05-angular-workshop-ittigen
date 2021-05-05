4d6546f67e3e9bc086d7bf7d2a852ec9231eeae1
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-05-05 11:40:02 +0200

    final typescript playground

--- typescript-playground/main.ts
+++ typescript-playground/main.ts
@@ -1,14 +1,4 @@
+import { Test } from "./test";
 
-
-class Test {
-
-    constructor(private antwort: number) {
-        console.log('😎', this.antwort);
-
-        const x = (param: number) => this.antwort + param;
-
-        console.log(x(1));
-    }
-}
 
 const test = new Test(42);
