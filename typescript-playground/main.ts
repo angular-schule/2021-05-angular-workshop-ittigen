5e3d835ca85ce36425063405a72132937cb105cb
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-05-05 11:07:17 +0200

    playground, fat arrows

--- typescript-playground/main.ts
+++ typescript-playground/main.ts
@@ -1,1 +1,14 @@
-console.log('😎')
+
+
+class Test {
+
+    constructor(private antwort: number) {
+        console.log('😎', this.antwort);
+
+        const x = (param: number) => this.antwort + param;
+
+        console.log(x(1));
+    }
+}
+
+const test = new Test(42);
