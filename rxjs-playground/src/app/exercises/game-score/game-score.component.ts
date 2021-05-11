89c52d5812b3f512f3f9bd09e9bbc5a21d204410
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-05-11 14:47:54 +0200

    rxjs: game-score with async pipe

--- rxjs-playground/src/app/exercises/game-score/game-score.component.ts
+++ rxjs-playground/src/app/exercises/game-score/game-score.component.ts
@@ -1,47 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { Subject, ReplaySubject } from 'rxjs';
-import { scan, reduce } from 'rxjs/operators';
+import { Component } from '@angular/core';
+import { Subject } from 'rxjs';
+import { reduce, scan } from 'rxjs/operators';
 
 @Component({
   selector: 'rxw-game-score',
   templateUrl: './game-score.component.html',
 })
-export class GameScoreComponent implements OnInit {
+export class GameScoreComponent {
 
-  logStream$ = new ReplaySubject<string | number>();
   score$ = new Subject<number>();
 
-  currentScore = 0;
-  finalScore: number;
+  currentScore$ =  this.score$.pipe(
+    scan((acc, item) => acc + item, 0)
+  );
 
-  // TODO
-  currentScore$ = null;
-  finalScore$ = null;
-
-  ngOnInit() {
-
-    /**
-     * Wir entwickeln ein spannendes Browser-Spiel!
-     * Jetzt fehlt nur noch der Code, um den aktuellen und den finalen Punktestand zu ermitteln...
-     */
-
-    /******************************/
-
-    this.score$.pipe(
-      scan((acc, item) => acc + item, 0)
-    ).subscribe(score => this.currentScore = score);
-
-    this.score$.pipe(
-      reduce((acc, item) => acc + item, 0)
-    ).subscribe(score => this.finalScore = score);
-
-    /******************************/
-
-    this.score$.subscribe({
-      next: e => this.logStream$.next(e),
-      complete: () => this.logStream$.next('✅ COMPLETE')
-    });
-  }
+  finalScore$ = this.score$.pipe(
+    reduce((acc, item) => acc + item, 0)
+  );
 
   finishGame() {
     this.score$.complete();
@@ -50,5 +25,4 @@ export class GameScoreComponent implements OnInit {
   addScore(amount: number) {
     this.score$.next(amount);
   }
-
 }
