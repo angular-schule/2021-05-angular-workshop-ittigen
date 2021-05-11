c50a2ee84150b63e78a5a30857be91633745c06b
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-05-11 11:17:06 +0200

    rxjs: fromEvent (learning operators)

--- rxjs-playground/src/app/exercises/fromevent/fromevent.component.ts
+++ rxjs-playground/src/app/exercises/fromevent/fromevent.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { fromEvent } from 'rxjs';
-import { map, startWith, debounceTime } from 'rxjs/operators';
+import { map, startWith, debounceTime, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'rxw-fromevent',
@@ -22,7 +22,14 @@ export class FromeventComponent implements OnInit {
 
     /******************************/
 
-    
+    fromEvent(window, 'resize').pipe(
+      map((e: Event) => (e.target as Window).innerWidth),
+      // map(() => window.innerWidth),
+      debounceTime(500),
+      startWith(window.innerWidth),
+      tap(console.log)
+    ).subscribe(width => this.currentWidth = width)
+
     /******************************/
   }
 
