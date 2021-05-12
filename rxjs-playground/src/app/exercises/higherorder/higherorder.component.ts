9d5efde737d025d9065ed14f9b12da2e651496fe
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-05-12 10:03:33 +0200

    rxjs: concatMap

--- rxjs-playground/src/app/exercises/higherorder/higherorder.component.ts
+++ rxjs-playground/src/app/exercises/higherorder/higherorder.component.ts
@@ -30,7 +30,7 @@ export class HigherorderComponent implements OnInit {
     /**************!!**************/
 
     this.result$ = this.source$.pipe(
-      mergeMap(tier => this.es.echo(tier))
+      concatMap(tier => this.es.echo(tier))
     );
 
     /**************!!**************/
